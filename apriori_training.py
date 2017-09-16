e89888441afb61d0f2b0ce2cef47fe5b9fc647ff
Hasan Ünlü <hu1997tr@gmail.com>
2017-09-16 19:08:40 +0300

    Completed Training

---
+++ apriori_training.py
@@ -0,0 +1,19 @@
+
+#Libraires
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+np.set_printoptions(threshold=np.inf)
+
+#Dataset importing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in  range(0,7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+    
+# Training apriori on the dataset
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003 , min_confidence = 0.2, min_lift = 3 , min_lenght = 2) 
+    
+#Visualizing the results
+results = list(rules)
